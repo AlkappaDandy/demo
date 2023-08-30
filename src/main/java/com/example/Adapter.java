68a982a37d531f301b6e5330bd8341baea5f8fc4
AlkappaDandy <dannyfahs@web.de>
2023-08-30 14:33:44 +0200

    init

---
+++ src/main/java/com/example/Adapter.java
@@ -0,0 +1,18 @@
+package com.example;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class Adapter{
+
+   public static List<Product> deserializRecipes(){
+      Gson gson = new Gson();
+      java.lang.reflect.Type productList = new TypeToken<ArrayList<Product>>(){}.getType();
+      List<Product> products = gson.fromJson(FileReader.readJsonFile("test.json"),productList);
+      //Recipe recipes = gson.fromJson(FileReader.readJsonFile("test.json"),Recipe.class);
+      return products;
+   }
+   
+}
+
